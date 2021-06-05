3f1f5c78b353df80a3d3ddfecb36ebfe9c95ef92
zubair-pc\zubair <szubair@gmail.com>
2021-06-05 18:26:40 +0500

    In main in this commit use of print and print line function added

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,14 @@
+/*
+println print the content and add end line
+on the other hand print function also prints but does not add new line
+*/
+fun basicUseOfPrintFunctions(){
 
+    println("Hello World with end line");
+    print("Hello World Allows next text to come on same line ");
+    println("Hello World comes on pervious line");
+}
 //Every kotlin Programme starts from main method
 fun main(){
-    println("Hello World");
+    basicUseOfPrintFunctions();
 }
