84db64144721ae6a747f5b700bb4e2b5d85be8c6
zubair-pc\zubair <szubair@gmail.com>
2021-06-05 18:30:16 +0500

    In this commit an integer function is printed with print functions

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,16 @@
 /*
 println print the content and add end line
 on the other hand print function also prints but does not add new line
+you can pass argument of any literal type to print functions
 */
 fun basicUseOfPrintFunctions(){
 
     println("Hello World with end line");
     print("Hello World Allows next text to come on same line ");
     println("Hello World comes on pervious line");
+    println(26);
 }
+
 //Every kotlin Programme starts from main method
 fun main(){
     basicUseOfPrintFunctions();
